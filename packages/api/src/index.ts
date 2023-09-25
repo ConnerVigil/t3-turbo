27bd7b3ba9b0cf819b1827c6fef7c08249f8cb34
Conner Vigil <cjvigil@live.com>
2023-09-25 16:39:10 -0600

    Going to deploy

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -4,12 +4,31 @@ import * as z from "zod";
 import { publicProcedure, router } from "./trpc";
 
 export const appRouter = router({
-  postList: publicProcedure.query(async () => {
+  /**
+   * Get all posts
+   */
+  getAll: publicProcedure.query(async () => {
     const posts = await prisma.post.findMany();
     return posts;
   }),
+
+  /**
+   * Get a single post by id
+   */
+  getOne: publicProcedure.input(z.object({ id: z.string() })).query((opts) => {
+    const { id } = opts.input;
+    return prisma.post.findUnique({
+      where: {
+        id: id,
+      },
+    });
+  }),
+
+  /**
+   * Create a new post
+   */
   addPost: publicProcedure
-    .input(z.object({ title: z.string(), content: z.string() }))
+    .input(z.object({ title: z.string().min(1), content: z.string().min(1) }))
     .mutation(async (opts) => {
       const { title, content } = opts.input;
       const post = await prisma.post.create({
@@ -18,6 +37,42 @@ export const appRouter = router({
           content,
         },
       });
+      return post;
+    }),
+
+  /**
+   * Update a post
+   */
+  updatePost: publicProcedure
+    .input(z.object({ id: z.string(), title: z.string(), content: z.string() }))
+    .mutation(async (opts) => {
+      const { id, title, content } = opts.input;
+      const post = await prisma.post.update({
+        where: {
+          id: id,
+        },
+        data: {
+          title,
+          content,
+        },
+      });
+
+      return post;
+    }),
+
+  /**
+   * Delete a post
+   */
+  deletePost: publicProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(async (opts) => {
+      const { id } = opts.input;
+      const post = await prisma.post.delete({
+        where: {
+          id: id,
+        },
+      });
+
       return post;
     }),
 });
