db9f5ebd1829b96ccebae2a7c60f52e427d1bc91
Conner Vigil <cjvigil@live.com>
2023-09-25 16:48:14 -0600

    debugging

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,7 +1,8 @@
 import type { AppType } from "next/app";
 import { trpc } from "../util/trpc";
+import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }): AppType {
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return <Component {...pageProps} />;
 }
 
